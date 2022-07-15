225928eecb7586edaf51f8218872f6109c0c2411
Oliver Enciso <oliverenciso07@gmail.com>
2022-07-15 20:15:19 +0800

    devnet

--- src/app/app-settings.ts
+++ src/app/app-settings.ts
@@ -1,6 +1,6 @@
 export class AppSettings {
     public testNetWSProviderEndpoint = 'wss://bootnode001.humidefi.com:443';
-    public devnetWSProviderEndpoint = 'wss://node001.humidefi.com:443';
+    public devnetWSProviderEndpoint = 'ws://localhost:9944';
     // public wsProviderEndpoint = 'wss://node001.humidefi.com:443';
     
     
